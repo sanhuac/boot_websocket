f08cec5b9b2c7cfea9e6dc735d610d8692352457
sanhuac <hucong123456>
2018-11-14 16:42:32 +0800

    通讯

---
+++ src/main/java/com/huc/boot_websocket/BootWebsocketApplication.java
@@ -0,0 +1,12 @@
+package com.huc.boot_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootWebsocketApplication.class, args);
+    }
+}
---
+++ src/test/java/com/huc/boot_websocket/BootWebsocketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huc.boot_websocket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootWebsocketApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
